4486f69ff8fbe51197580e9ebf1e6fe4ceb92af6
choiyeonseok <smr603@snu.ac.kr>
2019-12-18 17:19:03 +0900

    Add comment to main logic

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -11,28 +11,29 @@ public class LottoApplication {
 
     public static void main(String[] args) {
 
+        /** 1. 구매 금액 입력 */
         double purchaseAmount = InputView.inputPurchaseAmount();
         int numberOfSet = (int)purchaseAmount / UNIT_AMOUNT;
 
+        /** 2. 로또 번호 입력 */
         List<Lotto> lottoSet = new ArrayList<>();
         for (int i = 0; i < numberOfSet; i++){
             lottoSet.add(InputView.inputLottoNumbers());
         }
-
         OutputView.printLottoSet(numberOfSet, lottoSet);
 
-
+        /** 3. 금주 당첨 번호 입력 */
         List<Integer> winningNumbers = InputView.inputWinningNumbers();
         int bonusNumber = InputView.inputBonusNumber(winningNumbers);
         WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
 
-
-
+        /** 4. 결과출력을 위한 초기화 */
         Map<WinningResult, Integer> result = new HashMap<>();
         for (WinningResult rank : WinningResult.values()) {
             result.put(rank, 0);
         }
 
+        /** 5. 당첨 결과 계산 및 출력 */
         WinningResult winningResult;
         for (Lotto lotto : lottoSet) {
             winningResult = winningLotto.match(lotto);
@@ -40,12 +41,11 @@ public class LottoApplication {
         }
         OutputView.printResults(result);
 
+        /** 6. 수익률 계산 및 출력 */
         double earningRate = 0;
         for (WinningResult  rank : result.keySet()) {
             earningRate += (double)(rank.getWinningMoney()) * result.get(rank) / (numberOfSet * UNIT_AMOUNT) * PERCENTAGE;
         }
         OutputView.printEarningRate(earningRate);
-
-
     }
 }
